7f5061f0e5f69b5ebc9c7bcb9f6af3cf5f0bef64
Alba Mateos <albmatcar@gmail.com>
2025-05-26 17:23:47 +0100

    adds media query for bigger screens

--- src/pages/Layout.styled.ts
+++ src/pages/Layout.styled.ts
@@ -4,10 +4,14 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding-top: 5rem;
   margin: auto;
-  padding: 5rem;
   width: 100%;
   min-height: 100vh;
   box-sizing: border-box;
+
+  padding: 0.5rem;
+
+  @media (min-width: 1024px) {
+    padding: 2rem 5rem;
+  }
 `;
